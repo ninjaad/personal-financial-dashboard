c9d1f913a7d9f156d6dcd363d0952bc243c00955
ninjaad <aryandubey483@gmail.comm>
2025-05-13 01:18:49 +0530

    Connected to frontend

--- src/main/java/com/ninja/personal/financial/dashboard/controller/DashboardController.java
+++ src/main/java/com/ninja/personal/financial/dashboard/controller/DashboardController.java
@@ -3,6 +3,7 @@ package com.ninja.personal.financial.dashboard.controller;
 
 import com.ninja.personal.financial.dashboard.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/dashboard")
 public class DashboardController
--- src/main/java/com/ninja/personal/financial/dashboard/controller/TransactionController.java
+++ src/main/java/com/ninja/personal/financial/dashboard/controller/TransactionController.java
@@ -7,8 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/transactions")
 public class TransactionController {
